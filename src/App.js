a333c01a1d1cb2b75dc02fdbb5be358f56bcdc97
brionnedavis724 <brionne.davis724@gmail.com>
2023-04-13 00:05:54 -0400

    add Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
+import './index.css';
 import Header from "./components/Header"
 import Home from "./components/Home"
-
 
 function App() {
   return (
---
+++ src/components/Section.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Section() {
+  return (
+    <Wrap>
+        Section
+    </Wrap>  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+
+`
